5d849900d27e5f992013647914c839d49cc3082f
evanrich2404 <evanrich2404@gmail.com>
2023-03-31 17:31:49 -0500

    maybe this is finally good

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -31,29 +31,37 @@ def get_employee_todo_progress(employee_id):
             completed_task_titles.append(task['title'])
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, completed_tasks, total_tasks))
+          employee_name, completed_tasks, total_tasks))
     for title in completed_task_titles:
-        print("\t ", title)
+        print("\t {}", title)
 
-    with open('{}.csv'.format(employee_id), 'w',
-              encoding='utf-8', newline='') as csvfile:
-        fieldnames = ['USER_ID', 'USERNAME',
-                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_ALL)
+
+def export_to_csv(employee_id, employee_name, todo_data):
+    """exports data in the CSV format"""
+    file_name = "{}.csv".format(employee_id)
+
+    with open(file_name, 'w', encoding='utf-8', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
         for task in todo_data:
-            writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': employee_name,
-                'TASK_COMPLETED_STATUS': str(task['completed']).lower(),
-                'TASK_TITLE': task['title']
-            })
+            csv_writer.writerow([employee_id, employee_name,
+                                 task['completed'], task['title']])
+
+    print("Data exported to {}".format(file_name))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
-    else:
-        print("Usage: python script_name.py <employee_id>")
+    if len(sys.argv) != 2:
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    employee_data, todo_data = get_employee_todo_progress(employee_id)
+
+    if employee_data and todo_data:
+        export_to_csv(employee_id, employee_data, todo_data)
