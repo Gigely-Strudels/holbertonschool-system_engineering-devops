9d2d6bbb633d1aef5f97f5b168762ad16ee937a4
evanrich2404 <evanrich2404@gmail.com>
2023-03-31 17:39:21 -0500

    cleaned

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -7,44 +7,37 @@ and exports data in the CSV format.
 import requests
 import sys
 import csv
-
-EMPLOYEE_ENDPOINT = "https://jsonplaceholder.typicode.com/users/{}/"
-TODO_ENDPOINT = "https://jsonplaceholder.typicode.com/todos?userId={}"
+import urllib
 
 
 def get_employee_todo_progress(employee_id):
     """grabs employee todo progress"""
-    employee_response = requests.get(EMPLOYEE_ENDPOINT.format(employee_id))
-    employee_data = employee_response.json()
-    employee_name = employee_data['username']
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    todo_response = requests.get(TODO_ENDPOINT.format(employee_id))
+    employee_response = requests.get("{}/users/{}"
+                                     .format(base_url, employee_id))
+    employee_data = employee_response.json()
+
+    if "name" not in employee_data:
+        print("Employee ID does not exist")
+        return None, None
+
+    todo_response = requests.get("{}/users/{}/todos"
+                                 .format(base_url, employee_id))
     todo_data = todo_response.json()
 
-    total_tasks = len(todo_data)
-    completed_tasks = 0
-    completed_task_titles = []
-
-    for task in todo_data:
-        if task['completed']:
-            completed_tasks += 1
-            completed_task_titles.append(task['title'])
-
-    print("Employee {} is done with tasks({}/{}):".format(
-          employee_name, completed_tasks, total_tasks))
-    for title in completed_task_titles:
-        print("\t {}", title)
+    return employee_data, todo_data
 
 
 def export_to_csv(employee_id, employee_name, todo_data):
     """exports data in the CSV format"""
     file_name = "{}.csv".format(employee_id)
 
-    with open(file_name, 'w', encoding='utf-8', newline='') as csvfile:
+    with open(file_name, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
         for task in todo_data:
-            csv_writer.writerow([employee_id, employee_name,
+            csv_writer.writerow([employee_id, employee_data['username'],
                                  task['completed'], task['title']])
 
     print("Data exported to {}".format(file_name))
